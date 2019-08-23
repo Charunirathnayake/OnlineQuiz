ce0a3357d9e8caeb165cd92a783166e52e6b4aef
Charunirathnayake <charunirathnayake1995@gmail.com>
2019-08-23 23:02:05 +0530

    Online Quiz System
    Online Quiz system using html,css,js,php.

---
+++ Quiz/home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>HOME</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+<section class="sec-main">
+  <input id="nav-check" type="checkbox" />
+  <label for="nav-check">
+   <b> GO</b>
+  </label>
+  <div class="overlay">
+    <header role="banner">
+      <nav role="navigation">
+        <ul class="nav">
+          <li><a href="home.php"><i class="fa fa-home" style="font-size:36px"></i></a></li>
+          <li><a href="#"><i class="fa fa-user-plus" style="font-size:36px"></i></a></li>
+          <li><a href="log.php"><i class="fa fa-user-circle" style="font-size:36px"></i></a></li>
+          <li><a href="#"><i class="fa fa-power-off" style="font-size:36px"></i></a></li>
+        </ul>
+      </nav> 
+    </header>
+  </div>
+</section>
+</body>
+</html>
---
+++ Quiz/log.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>logIn</title>
+	<link rel="stylesheet" type="text/css" href="log.css">
+</head>
+<body>
+	<div class="loginbox">
+		<img src="Images/avatar.png" class="avatar">
+		<h1>Login Here</h1>
+		<form>
+			<p>Username</p>
+			<input type="text" name="user" placeholder="Enter Username">
+			<p>Password</p>
+			<input type="Password" name="Password" placeholder="Enter Password">
+			<input type="submit" name="" value="Login">
+			<a href="#">Lost Your Password?</a><br>
+			<a href="#">Don't have an account?</a>
+		</form>
+	</div>
+
+
+</body>
+</html>
